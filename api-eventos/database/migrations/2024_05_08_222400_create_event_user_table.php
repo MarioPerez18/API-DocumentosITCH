cbd479102dc5cb597033068939a31bb8c74bcaa8
MarioPerez18 <vapire117@gmail.com>
2024-09-10 09:27:52 -0500

    actualización en migracion event_user

--- api-eventos/database/migrations/2024_05_08_222400_create_event_user_table.php
+++ api-eventos/database/migrations/2024_05_08_222400_create_event_user_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('event_participant');
+        Schema::dropIfExists('event_user');
     }
 };
